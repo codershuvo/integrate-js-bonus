7badfebda6c7037d3743f12214d25cb9bdbae319
codershuvo <nisho.mushfiq2020@gmail.com>
2021-02-21 17:51:21 +0600

    bonus

--- arguments.js
+++ arguments.js
@@ -1,7 +1,12 @@
 function getFullName (firstName, lastName) {
-    const fullName = firstName + " " + lastName; 
+    console.log(arguments);
+    let fullName = " "; 
+    for (let i = 0; i < arguments.length; i++) {
+        const namePart = arguments[i];
+        fullName = fullName + " " + namePart; 
+    }
     return fullName; 
 }
 
-const name = getFullName('Hanif', 'Songket', 'Poribohon'); 
+const name = getFullName('Hanif', 'Songket', 'Poribohon', 'subokta', 'jonodorodi'); 
 console.log(name);
